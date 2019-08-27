09d586ed0682ee63fe9d4be8807bf93247a9b38e
tsabit12 <tsabit830@gmail.com>
2019-08-27 12:21:57 +0700

    remove pin

--- src/components/pages/ReqPickupPage.js
+++ src/components/pages/ReqPickupPage.js
@@ -4,7 +4,7 @@ import PropTypes from "prop-types";
 import ListReqPickup from "../list/ListReqPickup";
 import { connect } from "react-redux";
 import { fetchPickup, pickup } from "../../actions/handover";
-import { Segment, Header, Icon, Divider, Dimmer, Loader, Message, Modal, Form, Button } from "semantic-ui-react";
+import { Segment, Header, Icon, Divider, Dimmer, Loader, Message } from "semantic-ui-react";
 
 
 class ReqPickupPage extends React.Component{
@@ -12,7 +12,6 @@ class ReqPickupPage extends React.Component{
 		loading: false,
 		errors: {},
 		open: false,
-		pin: '',
 		data: {},
 		errorsPin: {}
 	}
@@ -22,62 +21,27 @@ class ReqPickupPage extends React.Component{
 	}
 
 	submit = (data) => {
-		this.setState({ open: true, data: data, pin: '' });
+		this.setState({ loading: true });
+		const datatosend = { data: data }
+		this.props.pickup(datatosend)
+		.then(() => this.setState({ loading: false }))
+		.catch(err => this.setState({ loading: false, errors: err.response.data.errors }));
 	}
-
-	onChange = (e) => this.setState({ pin: e.target.value})
 
 	close = () => this.setState({ open: false })
 
-	sumbitdata = () => {
-		const errorsPin = this.validate(this.state.pin);
-		this.setState({ errorsPin })
-		if (Object.keys(errorsPin).length === 0) {
-			this.setState({ loading: true, open: false });
-			const datatosend = { data: this.state.data, pin: this.state.pin }
-			console.log(datatosend);
-			this.props.pickup(datatosend)
-			.then(() => this.setState({ loading: false }))
-			.catch(err => this.setState({ loading: false, errors: err.response.data.errors }));
-		}
-	}
-
-	validate = (pin) => {
-		const errors = {};
-		if (pin === ""){
-			errors.pin = "Masukan pin";
-		}
-
-		return errors;
-	}
+	// sumbitdata = () => {
+	// 	this.setState({ loading: true});
+	// 	this.props.pickup(datatosend)
+	// 	.then(() => this.setState({ loading: false }))
+	// 	.catch(err => this.setState({ loading: false, errors: err.response.data.errors }));
+	// }
 
 	render(){
-		const { errors, errorsPin } = this.state;
-		console.log(this.state.data);
+		const { errors } = this.state;
+		
 		return(
 			<Navbar>
-				<Modal size="mini" open={this.state.open} onClose={this.close}>
-		          <Modal.Header>PIN</Modal.Header>
-		          <Modal.Content>
-		           	<Form>
-		           		<Form.Field>
-		           			<Form.Input 
-		           				name="pin"
-		           				id="pin"
-		           				type="password"
-		           				value={this.state.pin}
-		           				onChange={this.onChange}
-		           				palceholder= "Masukan pin"
-		           				error={ errorsPin.pin }
-		           			/>
-		           		</Form.Field>
-		           	</Form>
-		          </Modal.Content>
-		          <Modal.Actions>
-		            <Button negative onClick={this.close}>Tutup</Button>
-		            <Button secondary onClick={this.sumbitdata}>Submit</Button>
-		          </Modal.Actions>
-		        </Modal>
 				<Segment padded>
 				    { errors.global && <Message negative>
 						<Message.Header>Maaf!</Message.Header>
