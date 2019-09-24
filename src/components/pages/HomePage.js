12c2d3820b6e468b34d6d4257cedd11dc548e0f9
tsabit12 <tsabit830@gmail.com>
2019-09-24 14:17:36 +0700

    modify homepage

--- src/components/pages/HomePage.js
+++ src/components/pages/HomePage.js
@@ -1,9 +1,18 @@
 import React from "react";
 import Navbar from "../menu/Navbar";
+import { Segment, Image, Header, Icon, Divider } from "semantic-ui-react";
 
 const HomePage = () => (
 	<Navbar>
-		<h3>Hompage</h3>
+		<Segment padded='very'>
+			<Header as='h3'>
+			    <Header.Content>Selamat Datang</Header.Content>
+			</Header>
+			<Divider />
+			<Image src='https://react.semantic-ui.com/images/wireframe/paragraph.png' alt='paragraph' />
+			<br />
+			<Image src='https://react.semantic-ui.com/images/wireframe/paragraph.png' alt='paragraph' />
+		</Segment>
 	</Navbar>
 );
 
