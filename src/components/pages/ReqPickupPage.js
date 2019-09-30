10502c24caaaf3c71bd894967ccc24f0dbdac8cb
tsabit12 <tsabit830@gmail.com>
2019-09-30 11:08:06 +0700

    change radiobox with search dropdown

--- src/components/pages/ReqPickupPage.js
+++ src/components/pages/ReqPickupPage.js
@@ -1,12 +1,12 @@
 import React from "react";
 import Navbar from "../menu/Navbar";
 import PropTypes from "prop-types";
+import axios from "axios";
 import ListReqPickup from "../list/ListReqPickup";
 import { connect } from "react-redux";
 import { fetchPickup, pickup } from "../../actions/handover";
-import { getKantor } from "../../actions/order";
 import { setProgressBar } from "../../actions/progress";
-import { Header, Icon, Divider, Dimmer, Loader, Message, Modal, Button, Form, Checkbox } from "semantic-ui-react";
+import { Header, Icon, Divider, Dimmer, Loader, Message, Modal, Button, Form, Dropdown } from "semantic-ui-react";
 
 
 class ReqPickupPage extends React.Component{
@@ -16,7 +16,10 @@ class ReqPickupPage extends React.Component{
 		open: false,
 		data: {},
 		errorsPin: {},
-		value: ''
+		value: '',
+		options: [],
+		loadingSearch: false,
+		success: false
 	}
 
 	componentDidMount() {
@@ -26,50 +29,88 @@ class ReqPickupPage extends React.Component{
 
 	submit = (data) => {
 		this.setState({ open: true, data: data, value: '' });
-		this.props.getKantor();
 	}
 
 	close = () => this.setState({ open: false })
 
 	sumbitdata = () => {
-		if (!this.state.value) {
-			alert("Pilih salah satu");
-		}else{
-			this.setState({ loading: true});
-			const datatosend = {data: this.state.data, kantor: this.state.value }
+		const errors = this.validate(this.state.value);
+		this.setState({ errors });
+		if (Object.keys(errors).length === 0) {
+			this.setState({ loading: true, open: false });
+			const datatosend = { data: this.state.data, kantor: this.state.value }
+			
 			this.props.pickup(datatosend)
-			.then(() => this.setState({ open: false, loading: false }))
-			.catch(err => this.setState({ loading: false, open: false, errors: err.response.data.errors }));
+			.then(() => {
+				window.scrollTo(0, 0);
+				this.setState({ loading: false, success: true })
+			})
+			.catch(err => this.setState({ loading: false, errors: err.response.data.errors, success: false }));
 		}
+	}
+
+	validate = (value) => {
+		const errors = {};
+		if (!value) errors.kantor = "Kantor harap dipilih";
+		return errors;
 	}
 	
 	handleChange = (e, { value }) => this.setState({ value })
 
+	onSearchChange = (e, data) => {
+		clearTimeout(this.timer);
+		this.setState({ value: data.searchQuery });
+		this.timer = setTimeout(this.fetchKantor, 500);
+	}
+
+	fetchKantor = () => {
+		if (!this.state.value) return;
+		this.setState({ loadingSearch: true });
+		axios.post(`${process.env.REACT_APP_API}/Provinsi/getKantor`, {
+			kantor: this.state.value
+		})
+		.then(res => res.data.result)
+		.then(result => {
+			const options = [];
+			result.forEach(result => {
+				options.push({
+					key: result.kantorid,
+					value: result.kantorid,
+					text: result.namakantor
+				})
+			});
+			this.setState({ loadingSearch: false, options });
+		})
+		.catch(err => console.log(err));
+	}
+
+	onChangeKantor = (e, data) => this.setState({ value: data.value }) 
+
 	render(){
-		const { errors, open } = this.state;
-		const { datakantor } = this.props;
+		const { errors, open, success } = this.state;
 		
 		return(
 			<Navbar>
 				<Modal size="tiny" open={open}>
-		          <Modal.Header>Silahkan pilih kantor</Modal.Header>
-		          <Modal.Content scrolling>
-		            { datakantor.length === 0 ? <React.Fragment /> : 
-		            	<Form>
-		            		{datakantor.map(data => 
-		            			<Form.Field key={data.kantorid}>
-						          <Checkbox
-						            radio
-						            label={data.namakantor}
-						            name='checkboxRadioGroup'
-						            value={data.kantorid}
-						            checked={this.state.value === data.kantorid}
-						            onChange={this.handleChange}
-						          />
-						        </Form.Field>
-		            		)}
-		            	</Form>
-		            }
+		          <Modal.Header>Cari Kantor</Modal.Header>
+		          <Modal.Content>
+		            <Form onSubmit={this.sumbitdata}>
+		            	<Form.Field error={!!errors.kantor}>
+		            		<label>Kantor</label>
+		            		<Dropdown
+						        placeholder='Cari kantor pickup..'
+						        search
+						        selection
+						        fluid
+						        allowAdditions
+						        value={this.state.value}
+				    			onSearchChange={this.onSearchChange}
+				    			loading={this.state.loadingSearch}
+				    			options={this.state.options}
+				    			onChange={this.onChangeKantor}
+						    />
+		            	</Form.Field>
+		            </Form>
 		          </Modal.Content>
 		          <Modal.Actions>
 		            <Button negative onClick={this.close}>Tutup</Button>
@@ -82,14 +123,21 @@ class ReqPickupPage extends React.Component{
 		            />
 		          </Modal.Actions>
 		        </Modal>
+		        <Header as='h2'>
+				    <Icon name='envelope' />
+				    <Header.Content>Request Pickup</Header.Content>
+				</Header>
+
+		        { success && <Message positive>
+					<Message.Header>Sukses!</Message.Header>
+					<p>Request pickup berhasil di proses</p>
+				</Message> }
+
 			    { errors.global && <Message negative>
 					<Message.Header>Maaf!</Message.Header>
 					<p>{errors.global}</p>
 				</Message> }
-				<Header as='h2'>
-				    <Icon name='envelope' />
-				    <Header.Content>Request Pickup</Header.Content>
-				</Header>
+
 				<Divider/>
 				<Dimmer active={this.state.loading} inverted>
 			        <Loader inverted size='medium'>Loading</Loader>
@@ -105,10 +153,4 @@ ReqPickupPage.propTypes = {
 	pickup: PropTypes.func.isRequired
 }
 
-function mapStateToProps(state) {
-	return {
-		datakantor: state.order.kantor
-	}
-}
-
-export default connect(mapStateToProps, { fetchPickup, pickup, getKantor, setProgressBar })(ReqPickupPage);
+export default connect(null, { fetchPickup, pickup, setProgressBar })(ReqPickupPage);
