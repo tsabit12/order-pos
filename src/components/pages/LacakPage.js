a4b389c0aadf1cb74662c789dc1e07532daaf8fe
tsabit12 <tsabit830@gmail.com>
2019-10-30 08:32:49 +0700

    sort by event date desc

--- src/components/pages/LacakPage.js
+++ src/components/pages/LacakPage.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Navbar from "../menu/Navbar";
-import { Header, Divider, Input, Form, Message, Icon } from "semantic-ui-react";
+import { Divider, Input, Form, Message } from "semantic-ui-react";
 import InlineError from "../InlineError";
 import axios from "axios";
 import ListLacak from "../list/ListLacak";
@@ -13,6 +13,22 @@ class LacakPage extends React.Component {
 		trace: []
 	}
 
+	dynamicSort = (property) => {
+	    var sortOrder = 1;
+	    if(property[0] === "-") {
+	        sortOrder = -1;
+	        property = property.substr(1);
+	    }
+
+	    return function (a,b) {
+	        /* next line works with strings and numbers, 
+	         * and you may want to customize it to your needs
+	         */
+	        var result = (a[property] < b[property]) ? -1 : (a[property] > b[property]) ? 1 : 0;
+	        return result * sortOrder;
+	    }
+	}
+
 	handleClick = () => {
 		const errors = this.validate(this.state.barcode);
 		this.setState({ errors, loading: true });
@@ -20,6 +36,8 @@ class LacakPage extends React.Component {
 		axios.post(`${process.env.REACT_APP_API}/lacak`, { barcode })
 			.then(res => res.data.result)
 			.then(data => {
+				//sort by enventdate desc
+				data.sort(this.dynamicSort("-eventDate"));
 				this.setState({ errors: {}, trace: data, loading: false });
 			})
 			.catch(err => this.setState({ errors: err.response.data.errors, loading: false, trace: [] }))
@@ -42,10 +60,6 @@ class LacakPage extends React.Component {
 
 		return(
 			<Navbar>
-			    <Header as='h2'>
-			    	<Icon name="search" />
-			    	Lacak Kiriman
-			    </Header>
 			    <Form loading={this.state.loading}>
 			    	<Form.Field error={!!errors.barcode}>
 			    		<Input 
