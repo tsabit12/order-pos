811cd96af9b0ed08440335c8294a7e331c6925f9
tsabit12 <tsabit830@gmail.com>
2020-01-14 14:05:13 +0700

    grouping by id po

--- src/components/pages/PageListPo.js
+++ src/components/pages/PageListPo.js
@@ -1,45 +1,96 @@
 import React from "react";
 import PropTypes from "prop-types";
 import Navbar from "../menu/Navbar";
-// import ListData from "../po/ListData";
 import { connect } from "react-redux";
 import { fetchListpo, getTotalPage } from "../../actions/purchase";
 import { setProgressBar } from "../../actions/progress";
-//<ListData detailOrder={this.getDetailOrder} />
-import { Pagination, Table, Label, Header } from "semantic-ui-react";
+import { Pagination, Table, Label, Header, Sticky } from "semantic-ui-react";
+import "../css/Notfound.css";
 
 const numberWithCommas = (number) => {
 	return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
 
-const ListItems = ({ list }) => (
-	<React.Fragment>
-		{ list ? <React.Fragment>
-			{ list.map(x => <Table.Row 
-				key={x.RowNum} 
-				error={x.status === 'expired' ? true:false} 
-				positive={x.status === 'belum berlaku' ? true:false}
-				>
-					<Table.Cell>{x.RowNum}</Table.Cell>
-					<Table.Cell>{x.id_po}</Table.Cell>
-					<Table.Cell>Line ke {x.line}</Table.Cell>
-					<Table.Cell>{x.description}</Table.Cell>
-					<Table.Cell>{x.tgl_awal}</Table.Cell>
-					<Table.Cell>{x.tgl_akhir}</Table.Cell>
-					<Table.Cell style={{ textAlign: 'right'}}>{numberWithCommas(x.bsu_awal)}</Table.Cell>
-					<Table.Cell style={{ textAlign: 'right'}}>{numberWithCommas(x.sisa_saldo)}</Table.Cell>
-				</Table.Row>) }
-		</React.Fragment> : <Empty /> }
-	</React.Fragment>
-)
+const ListItems = ({ list }) => {
+	const contentTbody 	= [];
+	let groupingPo 		= '';
+	if (list) {
+		for(var i = 0; i < list.length; i++){
+			let item 		= list[i];
+			let valueIdpo 	= item.id_po; 
+			if (groupingPo !== valueIdpo) {
+				groupingPo = valueIdpo;
+				contentTbody.push(
+					<React.Fragment key={i}>
+						<Table.Row active>
+							<Table.Cell textAlign='left'>Nomor PO</Table.Cell>
+							<Table.Cell colSpan='5' textAlign='left'>{valueIdpo}</Table.Cell>
+						</Table.Row>
+						<Table.Row>
+							<Table.Cell>{item.line}</Table.Cell>
+							<Table.Cell>{item.description}</Table.Cell>
+							<Table.Cell
+								error={item.status === 'expired' ? true:false}
+								positive={item.status === 'belum berlaku' ? true:false}
+							>
+								{item.tgl_awal}
+							</Table.Cell>
+							<Table.Cell
+								error={item.status === 'expired' ? true:false}
+								positive={item.status === 'belum berlaku' ? true:false}
+							>
+								{item.tgl_akhir}
+							</Table.Cell>
+							<Table.Cell textAlign='right'>{numberWithCommas(item.bsu_awal)}</Table.Cell>
+							<Table.Cell textAlign='right'>{numberWithCommas(item.sisa_saldo)}</Table.Cell>
+						</Table.Row>
+					</React.Fragment>
+				);
+			}else{
+				contentTbody.push(
+					<Table.Row key={i}>
+						<Table.Cell>{item.line}</Table.Cell>
+						<Table.Cell>{item.description}</Table.Cell>
+						<Table.Cell
+							error={item.status === 'expired' ? true:false}
+							positive={item.status === 'belum berlaku' ? true:false}
+						>
+							{item.tgl_awal}
+						</Table.Cell>
+						<Table.Cell
+							error={item.status === 'expired' ? true:false}
+							positive={item.status === 'belum berlaku' ? true:false}
+						>
+							{item.tgl_akhir}
+						</Table.Cell>
+						<Table.Cell textAlign='right'>{numberWithCommas(item.bsu_awal)}</Table.Cell>
+						<Table.Cell textAlign='right'>{numberWithCommas(item.sisa_saldo)}</Table.Cell>
+					</Table.Row>
+				);
+			}
+		}
+	}else{
+		contentTbody.push(
+			<Empty />
+		);
+	}
+
+	return(
+		<React.Fragment>
+			{ contentTbody }
+		</React.Fragment>
+	)
+} 
 
 const Empty = () => (
 	<Table.Row>
-		<Table.Cell colSpan='8'>Data tidak ditemukan</Table.Cell>
+		<Table.Cell colSpan='6'>Data tidak ditemukan</Table.Cell>
 	</Table.Row>
 );
 
 class PageListPo extends React.Component {
+	contextRef = React.createRef();
+
 	state = {
 		loading: false,
 		pagination: {
@@ -83,52 +134,58 @@ class PageListPo extends React.Component {
 
 	render(){
 		const { pageTotal, items } = this.props;
-		const { page } = this.state.pagination;
+		const { page } 	= this.state.pagination;
+
 		return(
 			<Navbar>
-				<Header as='h2'>
-				    Purchase Order
-				    <Header.Subheader>
-				      Berikut adalah daftar purchase order anda
-				    </Header.Subheader>
-				</Header>
-				<Table singleLine>
-				    <Table.Header>
-				      <Table.Row>
-				        <Table.HeaderCell>NO</Table.HeaderCell>
-				        <Table.HeaderCell>NOMOR PO</Table.HeaderCell>
-				        <Table.HeaderCell>LINE</Table.HeaderCell>
-				        <Table.HeaderCell>DESKRIPSI</Table.HeaderCell>
-				        <Table.HeaderCell>TANGGAL AWAL</Table.HeaderCell>
-				        <Table.HeaderCell>TANGGAL AKHIR</Table.HeaderCell>
-				        <Table.HeaderCell style={{ textAlign: 'right'}}>TOTAL SALDO</Table.HeaderCell>
-				        <Table.HeaderCell style={{ textAlign: 'right'}}>SISA SALDO</Table.HeaderCell>
-				      </Table.Row>
-				    </Table.Header>
-				    <Table.Body>
-				    	{ Object.keys(items).length > 0 ? <ListItems list={items[`page${page}`]} /> : <Empty />}
-				    </Table.Body>
-				    <Table.Footer>
-						<Table.Row>
-							<Table.HeaderCell colSpan='3'>
-								<Label style={{backgroundColor: '#62823e', color: '#fff'}}>Belum berlaku</Label>
-								<Label style={{backgroundColor: '#db9686', color: '#fff'}}>Expired</Label>
-							</Table.HeaderCell>
-							<Table.HeaderCell colSpan='5' style={{textAlign: 'right'}}>
-								<Pagination 
-									boundaryRange={1}
-								    defaultActivePage={1}
-								    ellipsisItem={null}
-								    firstItem={null}
-								    lastItem={null}
-								    siblingRange={1}
-								    totalPages={pageTotal}
-								    onPageChange={this.onChange}
-								/>
-							</Table.HeaderCell>
-						</Table.Row>
-					</Table.Footer>
-				</Table>
+				<div ref={this.contextRef}>
+					<Sticky 
+			    		context={this.contextRef} 
+			    		offset={63}
+			    	>
+						<Header as='h3' block style={{borderRadius: 0, backgroundColor: '#FFF', borderLeft: '#FFF', borderRight: '#FFF', borderTop: '#FFF'}}>
+						    List Purchase Order
+						    <Header.Subheader>
+						      Berikut adalah daftar purchase order anda
+						    </Header.Subheader>
+						</Header>
+					</Sticky>
+					<Table className="fixed-header" celled>
+					    <Table.Header>
+					      <Table.Row>
+					        <Table.HeaderCell>LINE</Table.HeaderCell>
+					        <Table.HeaderCell>DESKRIPSI</Table.HeaderCell>
+					        <Table.HeaderCell>TANGGAL AWAL</Table.HeaderCell>
+					        <Table.HeaderCell>TANGGAL AKHIR</Table.HeaderCell>
+					        <Table.HeaderCell style={{ textAlign: 'right'}}>TOTAL SALDO</Table.HeaderCell>
+					        <Table.HeaderCell style={{ textAlign: 'right'}}>SISA SALDO</Table.HeaderCell>
+					      </Table.Row>
+					    </Table.Header>
+					    <Table.Body>
+					    	{ Object.keys(items).length > 0 ? <ListItems list={items[`page${page}`]} /> : <Empty />}
+					    </Table.Body>
+					    <Table.Footer>
+							<Table.Row>
+								<Table.HeaderCell colSpan='3'>
+									<Label style={{backgroundColor: '#62823e', color: '#fff'}}>Belum berlaku</Label>
+									<Label style={{backgroundColor: '#db9686', color: '#fff'}}>Expired</Label>
+								</Table.HeaderCell>
+								<Table.HeaderCell colSpan='5' style={{textAlign: 'right'}}>
+									<Pagination 
+										boundaryRange={1}
+									    defaultActivePage={1}
+									    ellipsisItem={null}
+									    firstItem={null}
+									    lastItem={null}
+									    siblingRange={1}
+									    totalPages={pageTotal}
+									    onPageChange={this.onChange}
+									/>
+								</Table.HeaderCell>
+							</Table.Row>
+						</Table.Footer>
+					</Table>
+				</div>
 			</Navbar>
 		);
 	}
